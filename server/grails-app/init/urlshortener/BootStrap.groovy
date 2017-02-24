99abf8c986d3825a2ff187549109eb9f35f57b58
Cameron Nicolle <cameron.nicolle.outlook.com>
2017-02-24 01:34:53 -0500

    Added shortened link

--- server/grails-app/init/urlshortener/BootStrap.groovy
+++ server/grails-app/init/urlshortener/BootStrap.groovy
@@ -3,6 +3,9 @@ package urlshortener
 class BootStrap {
 
     def init = { servletContext ->
+        // test link
+        def link = new ShortenedLink(destination: "https://youtube.com", shortName: "abc")
+        link.save()
     }
     def destroy = {
     }
